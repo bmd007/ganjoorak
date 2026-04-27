37e646422c45e187590f5ad1b2cd49d841505aff
Mahdi <bmd579@gmail.com>
2026-04-27 11:24:20 +0200

    clean

--- ganjoorak/frontend/vite.config.js
+++ ganjoorak/frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../src/main/resources/static',
-    emptyOutDir: false,
+    outDir: '../src/main/resources/static/vite/',
+    emptyOutDir: true,
   },
 })
